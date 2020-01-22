e80f631d87f62d613b2f37e1642887d7a650d723
Ilya Gavrilov <ilyagavrilov@mail.ru>
2020-01-22 16:23:11 +0300

    Homework 3. Прописал Scanner. test git.

--- src/lesson3/homework/Task1.java
+++ src/lesson3/homework/Task1.java
@@ -7,8 +7,12 @@ package lesson3.homework;
 выводится запрос – «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).
  */
 
+import java.util.Scanner;
+
 public class Task1 {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
