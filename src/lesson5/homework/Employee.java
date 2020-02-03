4f0e2d0da2bfaa3cf495ae705d5f7c037c178f3c
Ilya Gavrilov <ilyagavrilov@mail.ru>
2020-02-03 16:40:06 +0300

    Homework 5.

---
+++ src/lesson5/homework/Employee.java
@@ -0,0 +1,38 @@
+package lesson5.homework;
+
+
+public class Employee {
+    private String name;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+    public Employee(String name, String position, String email, String phoneNumber, int salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void employeeInfo() {
+        System.out.println("ФИО: " + name + "; Должность: " + position + "; Email: " + "; Телефон: " + phoneNumber + "; Зарплата: " + salary + "; Возраст: " + age + ".");
+    }
+
+    public static void main(String[] args) {
+        Employee employee = new Employee("Уткин Василий Петрович", "Сантехник", "отсутствует", "333-22-55", 14500, 71);
+
+        Employee[] employeesArray = new Employee[5];
+        employeesArray[0] = new Employee("Иванов Иван", "Junior developer", "ii@mail.ru", "123-45-67", 30000, 21);
+        employeesArray[1] = new Employee("Васильев Василий", "Middle Developer", "vv@mail.ru", "234-56-78", 35000, 35);
+        employeesArray[2] = new Employee("Андреев Андрей", "Senior Developer", "aa@mail.ru", "345-67-89", 40000, 41);
+        employeesArray[3] = new Employee("Ильин Илья", "Team lead", "ilil@mail.ru", "456-78-90", 50000, 48);
+        employeesArray[4] = new Employee("Петров Пётр", "CEO", "pp@mail.ru", "567-89-01", 100000, 53);
+
+        employee.employeeInfo();
+    }
+
+}
